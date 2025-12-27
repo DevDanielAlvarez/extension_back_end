29cab363140926eb58455306af1f25c5dc0f4439
daniel <skyrimdaniel11@gmail.com>
2025-12-27 20:42:17 -0300

    feat: add route to test api

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,15 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// ====> START V1 ROUTES <====
+Route::prefix('v1')->group(function () {
+    //route to test if the api is working
+    Route::get('ping', fn() => response()->json(['message' => 'pong (extension)']));
+    // ====> START PROTECTED ROUTES <====
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::get('/user', function (Request $request) {
+            return $request->user();
+        });
+    });
+});
+// ====> END V1 ROUTES <====
