bf9d8a31e14eca5220e73a8fbb4b7fa43b99ecf5
daniel <skyrimdaniel11@gmail.com>
2026-01-01 11:56:39 -0300

    feat: finish swagger doc to login and register methods

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,7 +1,13 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use OpenApi\Attributes as OA;
+#[OA\Info(
+    version: "1.0.0",
+    title: "Extension Api",
+    description: "Documentation of extension api"
+)]
+#[OA\Server(url: 'http://localhost:8000', description: 'Local Server')]
 abstract class Controller
 {
     //
