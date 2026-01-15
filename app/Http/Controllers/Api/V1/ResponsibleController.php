b73ee5600935a442d2eb6d3d8706feb4b5e24c2a
daniel <skyrimdaniel11@gmail.com>
2026-01-14 21:48:43 -0300

    feat: add index

---
+++ app/Http/Controllers/Api/V1/ResponsibleController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\ResponsibleResource;
+use App\Models\Responsible;
+use Illuminate\Http\Request;
+
+class ResponsibleController extends Controller
+{
+    public function index()
+    {
+        return ResponsibleResource::collection(Responsible::paginate(10));
+    }
+}
