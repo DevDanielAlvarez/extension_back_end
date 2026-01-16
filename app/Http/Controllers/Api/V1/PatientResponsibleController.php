b0960902e25ad59c135922ba0a146253c7bf6c31
daniel <skyrimdaniel11@gmail.com>
2026-01-16 14:15:20 -0300

    feat: add controller

---
+++ app/Http/Controllers/Api/V1/PatientResponsibleController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PatientResponsibleController extends Controller
+{
+    public function store(string $patientId, Request $request)
+    {
+    }
+}
