86a232c485b8bf01a1a54148bac2e5765abaa4fb
daniel <skyrimdaniel11@gmail.com>
2026-01-17 00:59:53 -0300

    feat: add migrate to pivot table

---
+++ database/migrations/2026_01_17_035812_create_patient_responsible_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('patient_responsible', function (Blueprint $table) {
+            $table->id();
+            $table->foreignUlid('patient_id')->constrained('patients')->onDelete('cascade');
+            $table->foreignUlid('responsible_id')->constrained('responsibles')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('patient_responsible');
+    }
+};
