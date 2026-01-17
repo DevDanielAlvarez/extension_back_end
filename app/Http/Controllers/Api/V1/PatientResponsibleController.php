e8e8684f5a7b5456af03944ca63499d9324cbb30
daniel <skyrimdaniel11@gmail.com>
2026-01-16 23:15:03 -0300

    feat: add json response

--- app/Http/Controllers/Api/V1/PatientResponsibleController.php
+++ app/Http/Controllers/Api/V1/PatientResponsibleController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\V1\Patient\AttachResponsibleRequest;
+use App\Http\Resources\Api\V1\PatientResource;
 use App\Services\PatientService;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class PatientResponsibleController extends Controller
 {
-    public function store(string $patientId, AttachResponsibleRequest $request)
+    public function store(string $patientId, AttachResponsibleRequest $request): JsonResponse
     {
         // get validated data from http request
         $data = $request->validated();
@@ -17,5 +19,10 @@ class PatientResponsibleController extends Controller
         $patient = PatientService::find($patientId);
         // attach responsible to patient
         $patient->getRecord()->responsibles()->attach($data['responsible_id']);
+        // return success response
+        return response()->json([
+            'message' => 'Responsible attached successfully.',
+            'data' => PatientResource::make($patient->getRecord()->load('responsibles'))
+        ], status: 200);
     }
 }
