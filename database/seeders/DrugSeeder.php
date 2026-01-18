8af1e42ee539f9f3182b2fe58b7933f561b74f05
daniel <skyrimdaniel11@gmail.com>
2026-01-18 00:15:27 -0300

    feat: add logic in seeder

--- database/seeders/DrugSeeder.php
+++ database/seeders/DrugSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Drug;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,6 @@ class DrugSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Drug::factory(10)->create();
     }
 }
