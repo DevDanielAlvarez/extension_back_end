9ff89e745c3e4f2e898c71f2fa4806468d2248e7
daniel <skyrimdaniel11@gmail.com>
2026-01-18 00:18:17 -0300

    feat: add responsible in data base seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,7 +18,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             RegistrationSequenceSeeder::class,
             DrugSeeder::class,
-            PatientSeeder::class
+            PatientSeeder::class,
+            ResponsibleSeeder::class
         ]);
     }
 }
